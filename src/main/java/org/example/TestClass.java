3582c8eeb99037b118b021575845fe8a81735f89
xuankim <kimxuan10a24@gmail.com>
2024-09-29 17:41:30 +0700

    run driver google

--- src/main/java/org/example/TestClass.java
+++ src/main/java/org/example/TestClass.java
@@ -1,13 +1,23 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 
 public class TestClass {
+    @BeforeMethod
+    public void run3(){
+        System.out.println("haha");
+        //set update data
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver chromeDriver = new ChromeDriver();
+        chromeDriver.get("https://www.google.com.vn/?hl=vi");
+    }
     @Test
     public void run() {
         System.out.println("hihi");
@@ -15,13 +25,10 @@ public class TestClass {
     @Test
     public void run1() {
         System.out.println("hehe");
-        Assert.assertEquals(1,2,"1 not equals to 2");
+        Assert.assertEquals(1,1,"1 not equals to 2");
+        //body testscript
     }
-    @BeforeMethod
-    public void run3(){
-        System.out.println("haha");
 
-    }
     @AfterMethod
     public void run4(){
         System.out.println("hmhm");
